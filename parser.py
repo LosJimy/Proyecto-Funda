bdfc8295458ded963edb910adec167638cd56c56
LosJimy <beo.erazoo@gmail.com>
2024-10-27 20:45:39 -0300

    Comentarios
    comentarios de que hacer

--- parser.py
+++ parser.py
@@ -13,7 +13,10 @@ def p_statement_expr(p): #Espresión para mostrar el desarrollo al revés, con e
 
 def p_expression_plus(p): #Expresión para sumar
     'expression : expression PLUS expression'
-    result = p[1]['result'] + p[3]['result']
+    #CAMBIO LUEGO BORRAR HACER QUE SEA
+    #3+4=7
+    #7=4+3
+    result = p[1]['result'] + p[3]['result'] 
     expr = f"{p[1]['expr']} + {p[3]['expr']}"
     p[0] = {'result': result, 'expr': expr}
 
@@ -23,17 +26,26 @@ def p_expression_number(p): #Expresión de un numero?
     
 def p_expression_minus(p): #Expresión para restar
     'expression : expression MINUS expression'
+    #CAMBIO LUEGO BORRAR HACER QUE SEA
+    #4-3=1
+    #1 = -3 + 4
     result = p[1]['result'] - p[3]['result']
     expr = f"{p[1]['expr']} - {p[3]['expr']}"
     p[0] = {'result': result, 'expr': expr}
     
 def p_expression_divide(p): #Expresión para dividir números
     'expression : expression DIVIDE expression'
+    #CAMBIO LUEGO BORRAR HACER QUE SEA
+    #25/5 = 5
+    #5 = 25 * 1/5
     result = p[1]['result'] / p[3]['result']
     expr = f"{p[1]['expr']} / {p[3]['expr']}"
     p[0] = {'result': result, 'expr': expr}
 
 def p_expression_multiply(p): #Expresión para multiplicar
+    #CAMBIO LUEGO BORRAR HACER QUE SEA
+    #3*4=12
+    #12=4*3
     'expression : expression MULTIPLY expression'
     result = p[1]['result'] * p[3]['result']
     expr = f"{p[1]['expr']} * {p[3]['expr']}"

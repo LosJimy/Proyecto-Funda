3c51f6dd2e88a934e2230b9d7eefc6039ac36df0
LosJimy <beo.erazoo@gmail.com>
2024-10-27 21:14:54 -0300

    Mejora de funciones
    mejore el sistema suma, resta, mult, y div

--- parser.py
+++ parser.py
@@ -9,52 +9,45 @@ precedence = (
 
 def p_statement_expr(p): #Espresión para mostrar el desarrollo al revés, con el resultado
     'statement : expression'
-    print(f"{p[1]['expr']} = {p[1]['result']}")
+    print(f"{p[1]['result']} = {p[1]['reversed_expr']}")
 
 def p_expression_number(p): #Expresión de un numero?
     'expression : NUMBER'
-    p[0] = {'result': p[1], 'expr': str(p[1])}
+    p[0] = {'result': p[1], 'expr': str(p[1]), 'reversed_expr': str(p[1])}
 
 def p_expression_plus(p): #Expresión para sumar
     'expression : expression PLUS expression'
-    #CAMBIO LUEGO BORRAR HACER QUE SEA
-    #3+4=7
-    #7=4+3
     result = p[1]['result'] + p[3]['result'] 
     expr = f"{p[1]['expr']} + {p[3]['expr']}"
-    p[0] = {'result': result, 'expr': expr}
+    reversed_expr = f"{p[3]['result']} + {p[1]['result']}"
+    p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
 
   
 def p_expression_minus(p): #Expresión para restar
     'expression : expression MINUS expression'
-    #CAMBIO LUEGO BORRAR HACER QUE SEA
-    #4-3=1
-    #1 = -3 + 4
+    #ARREGLAR ERROR
     result = p[1]['result'] - p[3]['result']
     expr = f"{p[1]['expr']} - {p[3]['expr']}"
-    p[0] = {'result': result, 'expr': expr}
+    reversed_expr = f"{p[3]['result']} - {p[1]['result']}"
+    p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
     
 def p_expression_divide(p): #Expresión para dividir números
     'expression : expression DIVIDE expression'
-    #CAMBIO LUEGO BORRAR HACER QUE SEA
-    #25/5 = 5
-    #5 = 25 * 1/5
     result = p[1]['result'] / p[3]['result']
     expr = f"{p[1]['expr']} / {p[3]['expr']}"
-    p[0] = {'result': result, 'expr': expr}
+    reversed_expr = f"{p[1]['result']} * 1/{p[3]['result']}"
+    p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
 
 def p_expression_multiply(p): #Expresión para multiplicar
-    #CAMBIO LUEGO BORRAR HACER QUE SEA
-    #3*4=12
-    #12=4*3
     'expression : expression MULTIPLY expression'
     result = p[1]['result'] * p[3]['result']
     expr = f"{p[1]['expr']} * {p[3]['expr']}"
-    p[0] = {'result': result, 'expr': expr}   
+    reversed_expr = f"{p[3]['result']} * {p[1]['result']}"
+    p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}   
     
 def p_expression_string(p): #Expresión de un String
     'expression : STRING'
-    p[0] = {'result': p[1][::-1], 'expr': p[1][::-1]}    
+    p[0] = {'result': p[1][::-1], 'expr': p[1][::-1], 'reversed_expr': p[1][::-1]}    
     
 def p_statement_end(p): #Función para terminar el programa con "END"
     'statement : END'
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftPLUSMINUSleftMULTIPLYDIVIDEDIVIDE END MINUS MULTIPLY NUMBER PLUS STRINGstatement : expressionexpression : expression PLUS expressionexpression : NUMBERexpression : expression MINUS expressionexpression : expression DIVIDE expressionexpression : expression MULTIPLY expressionexpression : STRINGstatement : END'
+_lr_signature = 'leftPLUSMINUSleftMULTIPLYDIVIDEDIVIDE END MINUS MULTIPLY NUMBER PLUS STRINGstatement : expressionexpression : NUMBERexpression : expression PLUS expressionexpression : expression MINUS expressionexpression : expression DIVIDE expressionexpression : expression MULTIPLY expressionexpression : STRINGstatement : END'
     
-_lr_action_items = {'END':([0,],[3,]),'NUMBER':([0,6,7,8,9,],[4,4,4,4,4,]),'STRING':([0,6,7,8,9,],[5,5,5,5,5,]),'$end':([1,2,3,4,5,10,11,12,13,],[0,-1,-8,-3,-7,-2,-4,-5,-6,]),'PLUS':([2,4,5,10,11,12,13,],[6,-3,-7,-2,-4,-5,-6,]),'MINUS':([2,4,5,10,11,12,13,],[7,-3,-7,-2,-4,-5,-6,]),'DIVIDE':([2,4,5,10,11,12,13,],[8,-3,-7,8,8,-5,-6,]),'MULTIPLY':([2,4,5,10,11,12,13,],[9,-3,-7,9,9,-5,-6,]),}
+_lr_action_items = {'END':([0,],[3,]),'NUMBER':([0,6,7,8,9,],[4,4,4,4,4,]),'STRING':([0,6,7,8,9,],[5,5,5,5,5,]),'$end':([1,2,3,4,5,10,11,12,13,],[0,-1,-8,-2,-7,-3,-4,-5,-6,]),'PLUS':([2,4,5,10,11,12,13,],[6,-2,-7,-3,-4,-5,-6,]),'MINUS':([2,4,5,10,11,12,13,],[7,-2,-7,-3,-4,-5,-6,]),'DIVIDE':([2,4,5,10,11,12,13,],[8,-2,-7,8,8,-5,-6,]),'MULTIPLY':([2,4,5,10,11,12,13,],[9,-2,-7,9,9,-5,-6,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -27,12 +27,12 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statement","S'",1,None,None,None),
-  ('statement -> expression','statement',1,'p_statement_expr','parser.py',15),
+  ('statement -> expression','statement',1,'p_statement_expr','parser.py',11),
+  ('expression -> NUMBER','expression',1,'p_expression_number','parser.py',15),
   ('expression -> expression PLUS expression','expression',3,'p_expression_plus','parser.py',19),
-  ('expression -> NUMBER','expression',1,'p_expression_number','parser.py',25),
-  ('expression -> expression MINUS expression','expression',3,'p_expression_minus','parser.py',29),
-  ('expression -> expression DIVIDE expression','expression',3,'p_expression_divide','parser.py',35),
-  ('expression -> expression MULTIPLY expression','expression',3,'p_expression_multiply','parser.py',41),
-  ('expression -> STRING','expression',1,'p_expression_string','parser.py',47),
-  ('statement -> END','statement',1,'p_statement_end','parser.py',51),
+  ('expression -> expression MINUS expression','expression',3,'p_expression_minus','parser.py',30),
+  ('expression -> expression DIVIDE expression','expression',3,'p_expression_divide','parser.py',40),
+  ('expression -> expression MULTIPLY expression','expression',3,'p_expression_multiply','parser.py',50),
+  ('expression -> STRING','expression',1,'p_expression_string','parser.py',60),
+  ('statement -> END','statement',1,'p_statement_end','parser.py',64),
 ]

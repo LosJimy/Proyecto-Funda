0f7bea178c6ff5a412d048feaea3d503172a8364
LosJimy <beo.erazoo@gmail.com>
2024-10-30 17:27:20 -0300

    arreglo en números simples
    input 1
    print 1
    input 15
    print 51

--- parser.py
+++ parser.py
@@ -9,47 +9,48 @@ precedence = (
 
 def p_statement_expr(p): #Espresión para mostrar el desarrollo al revés, con el resultado
     'statement : expression'
-    if isinstance(p[1], dict):
-        if 'reversed_expr' in p[1]:
-            print(f"{p[1]['result']} = {p[1]['reversed_expr']}")
-        else:
-            print(f"{p[1]['result']}")
+    if isinstance(p[1], dict) and 'reversed_expr' in p[1]:
+        print(f"{p[1]['result']} = {p[1]['reversed_expr']}")
     else:
         print(p[1])  
 
 def p_expression_number(p):
     '''expression : NUMBER
                   | FLOAT'''
-    p[0] = {'result': p[1], 'expr': str(p[1]), 'reversed_expr': str(p[1])}
-
+    inverted_number = str(p[1])[::-1] # invertir número
+    if isinstance(p[1], float):
+        p[0] = float(inverted_number)
+    else:
+        p[0] = int(inverted_number)
+        
 def p_expression_plus(p): #Expresión para sumar
     'expression : expression PLUS expression'
-    result = p[1]['result'] + p[3]['result'] 
-    expr = f"{p[1]['expr']} + {p[3]['expr']}"
-    reversed_expr = f"{p[3]['result']} + {p[1]['result']}"
+    result = p[1] + p[3] 
+    expr = f"{p[1]} + {p[3]}"
+    reversed_expr = f"{p[3]} + {p[1]}"
     p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
 
   
 def p_expression_minus(p): #Expresión para restar
     'expression : expression MINUS expression'
     #ARREGLAR ERROR
-    result = p[1]['result'] - p[3]['result']
-    expr = f"{p[1]['expr']} - {p[3]['expr']}"
-    reversed_expr = f"{p[3]['result']} - {p[1]['result']}"
+    result = p[1] - p[3]
+    expr = f"{p[1]} - {p[3]}"
+    reversed_expr = f"{p[3]} - {p[1]}"
     p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
     
 def p_expression_divide(p): #Expresión para dividir números
     'expression : expression DIVIDE expression'
-    result = p[1]['result'] / p[3]['result']
-    expr = f"{p[1]['expr']} / {p[3]['expr']}"
-    reversed_expr = f"{p[1]['result']} * 1/{p[3]['result']}"
+    result = p[1] / p[3]
+    expr = f"{p[1]} / {p[3]}"
+    reversed_expr = f"{p[1]} * 1/{p[3]}"
     p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
 
 def p_expression_multiply(p): #Expresión para multiplicar
     'expression : expression MULTIPLY expression'
-    result = p[1]['result'] * p[3]['result']
-    expr = f"{p[1]['expr']} * {p[3]['expr']}"
-    reversed_expr = f"{p[3]['result']} * {p[1]['result']}"
+    result = p[1] * p[3]
+    expr = f"{p[1]} * {p[3]}"
+    reversed_expr = f"{p[3]} * {p[1]}"
     p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}   
     
 def p_expression_string(p): #Expresión de un String

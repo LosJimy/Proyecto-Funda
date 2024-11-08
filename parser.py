7984cdcfce675e1f990366014208c92b94ccc405
LosJimy <beo.erazoo@gmail.com>
2024-11-08 13:01:41 -0300

    mejora de la suma

--- parser.py
+++ parser.py
@@ -9,19 +9,21 @@ precedence = (
 
 def p_statement_expr(p): #Espresión para mostrar el desarrollo al revés, con el resultado
     'statement : expression'
-    if isinstance(p[1], dict) and 'reversed_expr' in p[1]:
-        print(f"{p[1]['result']} = {p[1]['reversed_expr']}")
+    if isinstance(p[1], dict) and 'terms' in p[1]:
+        reversed_expr = ' + '.join(reversed(p[1]['terms']))
+        print(f"{p[1]['value']} = {reversed_expr}")
     else:
-        print(p[1])  
+        print(p[1])
 
 def p_expression_number(p):
     '''expression : NUMBER
                   | FLOAT'''
-    p[0] = p[1]
+    p[0] = {'value': p[1], 'terms': [str(p[1])]}
 
 def p_expression_neg(p):
     'expression : MINUS expression %prec MINUS'
-    p[0] = -p[2]
+    p[2]['value'] = -p[2]['value']
+    p[0] = p[2]
         
 def p_expression_binop(p):
     '''expression : expression PLUS expression
@@ -29,31 +31,24 @@ def p_expression_binop(p):
                   | expression MULTIPLY expression
                   | expression DIVIDE expression'''
     
-    left = p[1] if isinstance(p[1], (int, float)) else p[1]['result']
-    right = p[3] if isinstance(p[3], (int, float)) else p[3]['result']   
+    left = p[1]['value']
+    right = p[3]['value']
 
     if p[2] == '+':
         result = left + right
-        expr = f"{left} + {right}"
-        reversed_expr = f"{left} + {right}"
     elif p[2] == '-':
         result = left - right
-        expr = f"{left} - {right}"
-        reversed_expr = f"-{right} + {left}"
     elif p[2] == '*':
         result = left * right
-        expr = f"{left} * {right}"
-        reversed_expr = f"{right} * {left}"
     elif p[2] == '/':
         result = left / right
-        expr = f"{left} / {right}"
-        reversed_expr = f"{left} * 1/{right}"
             
-    p[0] = {'result': result, 'expr': expr, 'reversed_expr': reversed_expr}
+    terms = p[1]['terms'] + p[3]['terms']
+    p[0] = {'value': result, 'terms': terms}
     
 def p_expression_string(p): #Expresión de un String
     'expression : STRING'
-    p[0] = p[1][::-1]
+    p[0] = {'value': p[1][::-1], 'terms': [p[1][::-1]]}
     
 def p_statement_end(p): #Función para terminar el programa con "END"
     'statement : END'

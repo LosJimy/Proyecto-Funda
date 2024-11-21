ed264bdde23313240c11852c7a677fb889a61e6b
LosJimy <beo.erazoo@gmail.com>
2024-11-20 21:17:49 -0300

    Intento de Leer txt

--- severla.py
+++ severla.py
@@ -1,18 +1,14 @@
 from lexer import lexer
 from parser import parser
 
-statements = []
+def read_file(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+    return content
 
-def execute_statements():
-    global statements
+def execute_statements(statements):
     for statement in statements:
-        if statement:
-            if isinstance(statement, list):
-                for stmt in statement:
-                    execute_statement(stmt)
-            else:
-                execute_statement(statement)
-    statements = []
+        execute_statement(statement)
 
 def execute_statement(statement):
     if statement:
@@ -22,16 +18,25 @@ def execute_statement(statement):
         else:
             print(statement)
 
-while True:
-    try:
-        s = input('severla > ')
-    except EOFError:
-        break
+def main():
+    while True:
+        try:
+            s = input('severla > ')
+        except EOFError:
+            break
+        
+        if s.startswith('daer(') and s.endswith(')'):
+            filename = s[5:-1].strip()
+            content = read_file(filename)
+            result = parser.parse(content, lexer=lexer)
+            if result is not None:
+                execute_statements(result)
+        elif s.strip() == '':
+            continue
+        else:
+            result = parser.parse(s, lexer=lexer)
+            if result is not None:
+                execute_statements(result)
 
-    if s.strip() == "":
-        execute_statements()
-        continue
-
-    result = parser.parse(s, lexer=lexer)
-    if result is not None:
-        statements.append(result)
+if __name__ == '__main__':
+    main()

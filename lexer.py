bb752893423071fb780db15fa6da27479d0687f2
LosJimy <beo.erazoo@gmail.com>
2024-11-20 23:30:37 -0300

    Integración For y To
    funciona, pero no al 100%
    revisar mejor,

--- lexer.py
+++ lexer.py
@@ -22,6 +22,8 @@ tokens = (
     'ID',
     'IF',
     'ELSE',
+    'FOR',
+    'TO', #Palabra para el bucle for
     'END',
 
 )
@@ -54,6 +56,15 @@ def t_ELSE(t):
     t.type = 'ELSE'
     return t 
 
+def t_FOR(t):
+    r'rof|for'
+    t.type = 'FOR'
+    return t
+
+def t_TO(t):
+    r'to'
+    t.type = 'TO'
+    return t
 
 def t_END(t):
     r'END|End|end|SALIR|Salir|salir'
